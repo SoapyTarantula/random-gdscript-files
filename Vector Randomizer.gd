d2322043aed7a1f0536f349756af714e322394fe
SoapyTarantula <soapytarantula@gmail.com>
2022-10-27 05:32:56 -0500

    Create Vector Randomizer.gd
    A custom vector generator that can handle Vector2s or Vector3s with a bool deciding which one to return.

---
+++ Vector Randomizer.gd
@@ -0,0 +1,27 @@
+# Takes three floats and a bool to return a random Vector2 or Vector3 
+# with the floats as maximum possible values and the bool deciding
+# if it returns a Vector2 or Vector3. True means Vector3.
+
+# I made it because Godot doesn't have a way (that I know of at least) of directly
+# randomizing vectors, and I was getting tired of manually doing it each time.
+
+# Poorly designed by SoapyTarantula
+# https://github.com/SoapyTarantula | https://twitter.com/SoapyTarantula
+
+func RandomVector(xMax: float, yMax: float, zMax: float, isVector3: bool):
+	var randVec3
+	var randVec2
+
+	if isVector3:
+		randVec3 = Vector3(randf() * xMax, randf() * yMax, randf() * zMax)
+		randVec3.x = round(randVec3.xMax)
+		randVec3.y = round(randVec3.yMax)
+		randVec3.z = round(randVec3.zMax)
+	else:
+		randVec2 = Vector2(randf() * xMax, randf() * yMax)
+		randVec2.x = round(randVec2.xMax)
+		randVec2.y = round(randVec2.y)
+	if isVector3:
+		return Vector3(randVec3.xMax, randVec3.yMax, randVec3.zMax)
+	else:
+		return Vector2(randVec2.xMax, randVec2.yMax)
